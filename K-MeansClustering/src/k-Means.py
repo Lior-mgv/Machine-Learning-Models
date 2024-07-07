252a9670951e2112c270b9c4015bbac23695820c
Lior Mgv <s27212@pjwstk.edu.pl>
2024-07-07 18:02:17 +0200

    Unnecessary files removed

--- K-MeansClustering/src/k-Means.py
+++ K-MeansClustering/src/k-Means.py
@@ -1,58 +1,66 @@
-# import random
-# import numpy as np
-# from scipy.spatial import distance
-#
-# class DataPoint:
-#     def __init__(self, vector, data_class):
-#         self.vector = vector,
-#         self.data_class = data_class
-#
-#
-#
-# class KMeansClustering:
-#
-#     def __init__(self, n_clusters, data_points):
-#         self.n_clusters = n_clusters
-#         self.data_points = data_points
-#         self.dimension = len(self.data_points[0].vector)
-#         self.clusters = dict()
-#         for i in range(self.n_clusters):
-#             self.clusters[i] = []
-#         for data_point in data_points:
-#             self.clusters[random.randint(0, n_clusters-1)].append(data_point)
-#
-#     def calculate_centroid(self, cluster):
-#         centroid = []
-#         for i in range(self.dimension):
-#             for point in cluster:
-#                 centroid[i] += point.vector[i]
-#                 centroid[i] / len(cluster)
-#         return centroid
-#
-#
-#     def clusterize(self):
-#         while :
-#             centroids = []
-#             for cluster in self.clusters.values():
-#                 centroids.append(self.calculate_centroid(cluster))
-#             for data_point in self.data_points:
-#                 closest = distance.euclidean(centroids[0], data_point.vector)
-#                 index = 0
-#                 for centroid in centroids:
-#                     cur_distance = distance.euclidean(centroid, data_point.vector)
-#                     if cur_distance < closest:
-#                         closest = distance
-#                         index = centroids.index(centroid)
-#                 self.clusters[index].append(data_point)
-#
-# def read_from_file(file_name):
-#     data_points = []
-#     file = open(file_name, "r")
-#     for line in file:
-#         line_split = line.split(',')
-#         data_points.append(DataPoint(line_split[:-1], line_split[-1]))
-#     return data_points
-#
-#
-# KMeansClustering = KMeansClustering(4, read_from_file('iris.data'))
-#
+import random
+
+import numpy as np
+from scipy.spatial import distance
+
+
+
+class DataPoint:
+    vector = []
+
+    def __init__(self, vector, data_class):
+        self.vector = vector
+        self.data_class = data_class
+
+
+def read_from_file(file_name):
+    data_points = []
+    file = open(file_name, "r")
+    for line in file:
+        line_split = line.split(',')
+        data_points.append(DataPoint([float(i) for i in line_split[:-1]], line_split[-1]))
+    return data_points
+
+
+class KMeansClustering:
+
+    def __init__(self, k):
+        self.k = k
+
+    def clusterize(self, data_set):
+        centroids = {}
+        for i in range(self.k):
+            centroids[i] = np.array(data_set[random.randint(0, len(data_set) - 1)])
+
+        count = 0
+        while True:
+            clusters = {}
+            for i in range(self.k):
+                clusters[i] = []
+            for data_point in data_set:
+                distances = []
+                for centroid in centroids.values():
+                    distances.append(distance.euclidean(data_point, centroid))
+                clusters[distances.index(np.min(distances))].append(data_point)
+
+            total_distance = 0
+            for i in range(self.k):
+                total_distance += sum([distance.euclidean(x, centroids[i]) for x in clusters[i]])
+
+            print(f'Iteration {count}: {total_distance}')
+            old_centroids = centroids.copy()
+            centroids_equal = True
+            for i in range(self.k):
+                centroids[i] = np.mean(clusters[i], axis=0)
+                if np.any(centroids[i] != old_centroids[i]):
+                    centroids_equal = False
+            count += 1
+            if centroids_equal:
+                return clusters
+
+
+if __name__ == "__main__":
+    data_points = read_from_file("iris.data")
+    k_means = KMeansClustering(3)
+    clusters = k_means.clusterize([point.vector for point in data_points])
+    print(clusters)
